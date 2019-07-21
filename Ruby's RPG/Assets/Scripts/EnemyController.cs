f6aae6c2984ccd3e1a15a2b8b793a54834cfbebb
fedeiar <>
2019-07-21 17:46:55 -0300

    1 commit

---
+++ Ruby's RPG/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    public ParticleSystem smokeEffect;
+
+    
+    Rigidbody2D rigidbody2D;
+    Animator animator;
+    float timer;
+    int direction = 1;
+    bool broken;
+    AudioSource walkSound;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+        broken = true;
+        walkSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (!broken){
+            return;
+        }
+        
+  
+        timer -= Time.deltaTime;
+        Console.WriteLine("timer: " + timer);
+
+        if (timer < 0) {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical){
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
+
+        }
+        else{
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+        walkSound.Stop();
+    }
+}
